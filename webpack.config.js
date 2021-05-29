b7f3214375a592abdd8295e120a6a1d5f51e2d98
Aseeva Julia <j.s.aseeva@mail.ru>
2021-05-29 21:14:58 +0300

    initial build

---
+++ webpack.config.js
@@ -0,0 +1,99 @@
+const path = require('path');
+const HTMLWebpackPlugins = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const NODE_ENV = process.env.NODE_ENV;
+const isProd = NODE_ENV === 'production';
+
+module.exports = {
+  mode: NODE_ENV ? NODE_ENV : 'development',
+
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+  },
+
+  entry: path.resolve(__dirname, 'src/index.ts'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+
+  watch: true,
+  watchOptions: {
+    ignored: /node_modules/,
+    poll: 1000,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.[tj]sx?$/,
+        exclude: [/node_modules/],
+        use: ['ts-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-modules-typescript-loader?module',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                mode: 'local',
+                localIdentName: '[name]__[local]__[hash:base64:5]',
+                auto: /\.module\.\w+$/i,
+              },
+            },
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack', 'url-loader'],
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot)$/,
+        use: 'file-loader',
+      },
+      {
+        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+        use: ['url-loader'],
+      },
+    ],
+  },
+
+  plugins: [
+    new HTMLWebpackPlugins({
+      template: path.resolve(__dirname, 'public/index.html'),
+      minify: {
+        collapseWhitespace: isProd,
+      },
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'public/favicon.ico',
+          to: path.resolve(__dirname, 'dist'),
+        },
+        {
+          from: 'public/manifest.json',
+          to: path.resolve(__dirname, 'dist'),
+        },
+      ],
+    }),
+  ],
+
+  devServer: {
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+
+  devtool: 'source-map',
+};
