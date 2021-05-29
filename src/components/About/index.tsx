938d46357bf337d787383bbcdb24f2375205d569
Aseeva Julia <j.s.aseeva@mail.ru>
2021-05-30 02:00:46 +0300

    added info on sidebar

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -3,7 +3,21 @@ import React from 'react';
 import as from './About.module.scss';
 
 function AboutInfo() {
-  return <div className={as.wrapper}>AboutInfo</div>;
+  return (
+    <div className={as.wrapper}>
+      <div className={as.blockInfo}>
+        <h2>Profile</h2>
+        <p>
+          Front-end engineer with 2 years of web development experience in a diverse team (and 6 years of experience as
+          an engineer in the past). In last company I set up the frontend development process within the department,
+          participated in the full development cycle of new functionality, integrated the service into a mobile
+          application. Work hand-in-hand with designers, web-core team, backend developers and marketing team. I am
+          looking for a company where I can realize my passion for user interface development, grow as an engineer and
+          bring maximum benefit to buisness and users.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default AboutInfo;
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,26 +1,67 @@
 import React from 'react';
+import cn from 'classnames';
 
 import ss from './Sidebar.module.scss';
+
+import avatarImg from '../../assets/images/avatar.jpg';
+import githubSvg from '../../assets/icons/github.svg';
+import linkedIn from '../../assets/icons/linkedIn.svg';
+import facebook from '../../assets/icons/facebook.svg';
+import phone from '../../assets/icons/phone.svg';
+import envelop from '../../assets/icons/envelop.svg';
+import telegram from '../../assets/icons/telegram.svg';
 
 function SidebarInfo() {
   return (
     <div className={ss.wrapper}>
-      <div>
-        <div>Avatar</div>
-        <p>***</p>
-        <div>Iuliia Aseeva</div>
+      <div className={ss.avatarWrapper}>
+        <div className={ss.imageWrapper}>
+          <img src={avatarImg} alt="frontend developer avatar" className={ss.imageSrc} />
+        </div>
+        <h2 className={ss.userName}>Iuliia Aseeva</h2>
+        <div className={ss.userJob}>Frontend Engeneer</div>
+        {/* <p>***</p> */}
       </div>
-      <div>
-        <h2>Details</h2>
-        <div>Adress</div>
+      <div className={ss.detailsWrapper}>
+        <h3>Details</h3>
+        <div>Moscow</div>
       </div>
-      <div>
-        <h2>Contacts</h2>
+      <div className={ss.contacts}>
+        <h3>Contacts</h3>
         <div>
-          <div>1</div>
-          <div>2</div>
-          <div>3</div>
-          <div>4</div>
+          <div className={ss.contactsItem}>
+            <img src={phone} className={cn(ss.contactsLinkSvg, ss['contactsLinkSvg--phone'])} />
+            <p>+ 7 916 136 69 87</p>
+          </div>
+          <div className={ss.contactsItem}>
+            <img src={envelop} className={cn(ss.contactsLinkSvg, ss['contactsLinkSvg--phone'])} />
+            <p>aseeva.ays@gmail.com</p>
+          </div>
+          <div className={ss.socialAccs}>
+            <div className={ss.contactsItem}>
+              <a href="https://github.com/JulsDev" target="_blank" className={ss.contactsLink}>
+                <img src={githubSvg} className={ss.contactsLinkSvg} />
+              </a>
+            </div>
+            <div className={ss.contactsItem}>
+              <a href="https://www.linkedin.com/in/julia-aseeva-8a1751171" target="_blank" className={ss.contactsLink}>
+                <img src={linkedIn} className={ss.contactsLinkSvg} />
+              </a>
+            </div>
+            <div className={ss.contactsItem}>
+              <a
+                href="https://www.facebook.com/profile.php?id=100013984274634"
+                target="_blank"
+                className={ss.contactsLink}>
+                <img src={facebook} className={ss.contactsLinkSvg} />
+              </a>
+            </div>
+            <div className={ss.contactsItem}>
+              <a href="https://t.me/julsdev" target="_blank" className={ss.contactsLink}>
+                <img src={telegram} className={ss.contactsLinkSvg} />
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
